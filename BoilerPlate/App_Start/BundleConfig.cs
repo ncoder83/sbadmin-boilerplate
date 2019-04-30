689c3b2b6fb96a8168465dd0e0cdb6b4d44f8114
Pascal Anglade <pascal.nglade@gmail.com>
2019-04-30 08:42:21 -0400

    Initial commit

---
+++ BoilerPlate/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BoilerPlate
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            //add sb admin 2 js 
+            bundles.Add(new ScriptBundle("~/bundles/sbadmin").Include(
+                        "~/Scripts/sb-admin-2.js"));
+
+            //add a bundle for the chart library
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                        "~/Scripts/Chart.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                      "~/Content/bootstrap.css"));
+
+            //add sb admin 2 css
+            bundles.Add(new StyleBundle("~/Content/sbadmin2").Include(
+                      "~/Content/all.css", 
+                      "~/Content/sb-admin-2.css"));
+
+        }
+    }
+}
---
+++ BoilerPlate/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoilerPlate.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Components
+        public ActionResult ComponentButtons()
+        {
+            return View();
+        }
+
+        public ActionResult ComponentCards()
+        {
+            return View();
+        }
+
+
+        // Utilities
+        public ActionResult UtilitiesColors()
+        {
+            return View();
+        }
+
+        public ActionResult UtilitiesBorders()
+        {
+            return View();
+        }
+
+        public ActionResult UtilitiesAnimations()
+        {
+            return View();
+        }
+
+        public ActionResult UtilitiesOther()
+        {
+            return View();
+        }
+
+
+
+    }
+
+}
+
